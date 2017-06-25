222480081a85f07556447e548d7328bbf011a4b3
erdelf <erdelf.productions@gmail.com>
2017-06-26 00:20:05 +0200

    initial upload

---
+++ BackstoryEditDef.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BackstoryEditor
+{
+    public class BackstoryEditDef : Def
+    {
+        public string backstoryName;
+        public string backstoryDescription;
+
+
+        public override void ResolveReferences()
+        {
+            base.ResolveReferences();
+            if (BackstoryDatabase.TryGetWithIdentifier(this.backstoryName, out Backstory bs))
+                bs.baseDesc = this.backstoryDescription;
+        }
+    }
+}
